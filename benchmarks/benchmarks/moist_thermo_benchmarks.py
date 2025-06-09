b9036505ee9be34d366a7346d35fb58923f89fda
Jaye Norman <sjnorma2@ncsu.edu>
2025-06-09 14:48:02 -0600

    Updating moist benchmarks & cleaning files

--- benchmarks/benchmarks/moist_thermo_benchmarks.py
+++ benchmarks/benchmarks/moist_thermo_benchmarks.py
@@ -1,88 +1,50 @@
-import numpy as np
+import os
 import xarray as xr
 
 import metpy.calc as mpcalc;  
-from metpy.units import units
 
-def index_xarray_data():
-     """Create data for testing that index calculations work with xarray data."""
-     pressure = xr.DataArray([850., 700., 500.], dims=('isobaric',), attrs={'units': 'hPa'})
-     temp = xr.DataArray([[[[296., 295., 294.], [293., 292., 291.]],
-                           [[286., 285., 284.], [283., 282., 281.]],
-                           [[276., 275., 274.], [273., 272., 271.]]]] * units.K,
-                         dims=('time', 'isobaric', 'y', 'x'))
-     
-     mixingRatio = xr.DataArray([[[[9., 9., 9.], [10., 10., 10.]],
-                           [[11., 11., 11.], [11., 11., 11.]],
-                           [[12., 12., 270.], [12., 12., 12.]]]],
-                         dims=('time', 'isobaric', 'y', 'x'))
-     
-     vaporPressure = xr.DataArray([[[[30., 30., 30.], [28.,28.,28.]], 
-                                     [[27., 26., 27.], [25., 24., 24.]],
-                                     [[23., 19., 23.], [18., 16., .20]]]] * units.hPa,
-                                   dims=('time', 'isobaric', 'y', 'x'))
-     rh = xr.DataArray([[[[30., 30., 30.], [28.,28.,28.]], 
-                                     [[27., 26., 27.], [25., 24., 24.]],
-                                     [[23., 19., 23.], [18., 16., .20]]]] * units.percent,
-                                   dims=('time', 'isobaric', 'y', 'x'))
-     
-     dewpoint = xr.DataArray([[[[250., 250., 250.], [240., 240., 240.]],
-                           [[260., 260., 260.], [240., 240., 240.]],
-                           [[250., 250., 250.], [260., 260., 260.]]]] * units.K,
-                         dims=('time', 'isobaric', 'y', 'x'))
-     
-     return xr.Dataset({'temperature': temp,  'mixingRatio': mixingRatio, 'vaporPressure': vaporPressure,
-                        'pressure': pressure, 'relativeHumidity': rh, 'dewpoint':dewpoint},
-                       coords={'isobaric': pressure, 'time': ['2020-01-01T00:00Z']}); 
 
 class TimeSuite: 
     #NOTE: I'm using CalVer https://calver.org/ YYYY.MM.DD
-    version = "2025.06.03"; 
+    version = "2025.06.09"; 
     
-    def setup(self): 
-        self.mixingRatio = np.array([8, 7, 9, 10, 11, 12, 13, 14, 15, 16]); #dimensionless
-        self.t = np.array([22.2, 14.6, 12., 9.4, 7., -38., 13.3, 12.7, 2.3, 23.4]) * units.celsius
-        self.randomT = np.random.uniform(low=0.0, high=24.0, size=100) * units.celsius; 
-        self.randomMixingRatio = np.random.uniform(low = 0.0, high = 40.0, size=100); #dimensionless
-        self.pressure = np.linspace(1000, 1, 100) * units.hPa; 
-        self.randomTd = np.random.uniform(low = 0.0, high = 20.0, size=100) * units.degC
-        self.ds = index_xarray_data()
-        self.slice = self.ds.isel(isobaric=0)
+    def setup_cache(self):
+       base_path = os.path.dirname(__file__) # path to current file
+       file_path = os.path.join(base_path, "..", "data_array_compressed.nc");
+       file_path = os.path.abspath(file_path)
+       ds = xr.open_dataset(file_path)
+       return ds; 
+   
+    def setup(self, ds):
+       self.pressureSlice = ds.isel(pressure = 0, time = 0)
+       self.timeSlice = ds.isel(time = 0)
+       self.upperSlice = ds.isel(pressure = 49, time = 0)
         
-    def time_virtual_temperature(self): 
-        """Benchmark virtual temperature for one value."""
-        mpcalc.virtual_temperature(self.t[0], self.mixingRatio[0] * units("g/kg"));
+    def time_virtual_temperature(self, timeSlice): 
+        """Benchmark virtual temperature on a 3d cube."""
+        mpcalc.virtual_temperature(self.timeSlice.temperature, self.timeSlice.mixing_ratio);
         
+    def time_dewpoint(self, timeSlice): 
+        """Benchmarking dewpoint from vapor pressure on a 3d cube"""
+        mpcalc.dewpoint(self.timeSlice.vapor_pressure); 
         
-    def time_virtual_temperature_100_values(self): 
-        """Benchmarking the virtual temperature for 100 array values"""
-        mpcalc.virtual_temperature(self.randomT, self.randomMixingRatio * units("g/kg")); 
+    def time_rh_from_mixing_ratio(self, timeSlice):
+        """Benchmarking relative humidity from mixing ratio on a 3d cube"""
+        mpcalc.relative_humidity_from_mixing_ratio(self.timeSlice.pressure, self.timeSlice.temperature, self.timeSlice.mixing_ratio); 
     
-    def time_virtual_temperature_grid(self): 
-        """Benchmark virtual temperature on a grid"""
-        mpcalc.virtual_temperature(self.slice.temperature, self.slice.mixingRatio * units("g/kg")); 
+    def time_dewpoint_from_rh(self, timeSlice):
+        """Benchmarking dewpoint from calculated on a 3d cube"""
+        mpcalc.dewpoint_from_relative_humidity(self.timeSlice.temperature, self.timeSlice.relative_humidity);
         
-    def time_dewpoint_grid(self): 
-        """Benchmarking dewpoint from vapor pressure on a grid"""
-        mpcalc.dewpoint(self.slice.vaporPressure); 
+    def time_precipitable_water(self, timeSlice): 
+        """Benchmarking precipitable water calculation for one column"""
+        mpcalc.precipitable_water(self.timeSlice.pressure, self.timeSlice.dewpoint[0][0]);
         
-    def time_rh_from_mixing_ratio_grid(self):
-        """Benchmarking relative humidity from mixing ratio on a grid"""
-        mpcalc.relative_humidity_from_mixing_ratio(self.slice.pressure, self.slice.temperature, self.slice.mixingRatio); 
-    
-    def time_dewpoint_from_rh_grid(self):
-        """Benchmarking relative humidity from calculated rh on a grid"""
-        mpcalc.dewpoint_from_relative_humidity(self.slice.temperature, self.slice.relativeHumidity);
-        
-    def time_precipitable_water(self): 
-        """Benchmarking precipitable water calculation"""
-        mpcalc.precipitable_water(self.pressure, self.randomTd);
-        
-    def time_wet_bulb_temperature_grid(self):
-        """Benchmarking wet bulb temperature calculation"""
-        mpcalc.wet_bulb_temperature(self.pressure[0], self.t[0], self.randomTd[0]); 
+    def time_wet_bulb_temperature(self, pressureSlice):
+        """Benchmarking wet bulb temperature calculation on on a cube"""
+        mpcalc.wet_bulb_temperature(self.pressureSlice.pressure, self.pressureSlice.temperature, self.pressureSlice.dewpoint); 
         
         
-    def time_scale_height(self): 
-        """Benchmarking the calculation for the scale height of a layer"""
-        mpcalc.scale_height(self.t[0], self.t[5]); 
+    def time_scale_height(self, pressureSlice): 
+        """Benchmarking the calculation for the scale height of a layer for 2 surfaces"""
+        mpcalc.scale_height(self.upperSlice.temperature, self.pressureSlice.temperature); 
--- benchmarks/data_array_generate.py
+++ benchmarks/data_array_generate.py
@@ -1,11 +1,9 @@
-
+import os; 
 import xarray as xr
 import numpy as np; 
 import pandas as pd; 
 import metpy.calc as mpcalc
-import metpy.constants as mpconst
 from metpy.units import units
-import matplotlib.pyplot as plt
 
 """Create a sample xarray Dataset with 2D variables."""
 """Originally from cbook, modified by sjnorman for 3D"""
@@ -16,7 +14,7 @@ lons = np.linspace(360 - 100, 360 - 90, 50)
 pressure = np.linspace(250, 1000, 50) * units.hPa
 p_3d = pressure[:, np.newaxis, np.newaxis]
 
-times = pd.date_range("2024/01/01", "2024/06/01" ,freq='D');
+times = pd.date_range("2024/01/01", "2024/06/01" ,freq='M');
 
 #Adding height
 z = mpcalc.pressure_to_height_std(p_3d); 
@@ -65,23 +63,28 @@ for i, tm in enumerate(times):
 days = np.arange(len(times)); 
 annual_cycle = np.sin(2 * np.pi * days / len(times)); 
 
-seasonal_amplitude = 10 #degrees C difference from summer to winter
+seasonal_amplitude = 10 + 273.15;  #K difference from summer to winter
 seasonal_variation = seasonal_amplitude * annual_cycle; 
 
 
+lapse_rate = 6.5 * units("K/km"); #avg env gamma
+
 # make t as colder air to the north and 3d
 t_sfc = (np.linspace(15, 5, 50) * np.ones((50, 50)))
+t_sfc = (t_sfc + 273.15);  
+
 t_3d = np.zeros((len(pressure), 50, 50)) # (pressure, lat, lon) 
 for i, p in enumerate(pressure):
-    t_3d[i, :, :] = t_sfc * (p/1000) * mpconst.R / mpconst.Cp_d
+    t_3d[i, :, :] = (t_sfc * units.K) - (lapse_rate * height[i, :, :])
     
         
 #Make t colder in the winter, warmer in the summer         
 t_4d = np.zeros((50, 50, 50, len(times)))
 for i, tm in enumerate(times):
-    t_4d[:, :, :, i] = t_3d + seasonal_variation[i];     
+    t_4d[:, :, :, i] = t_3d + seasonal_variation[i]; 
+
+t_4d = t_4d * units.K;  
     
-t_4d = (t_4d + 273.15) * units.K
 
 
 #Generate potential temperature
@@ -106,6 +109,29 @@ for i, l in enumerate(lats):
 mixingRatio_4d = np.zeros((50, 50, 50, len(times)))
 for i, tm in enumerate(times): 
     mixingRatio_4d[:, :, :, i] = mixingRatio_3d; 
+    
+
+#Generate vapor pressure
+vapor_pressure_4d = mpcalc.vapor_pressure(p, mixingRatio_4d); 
+
+#Generate dewpoint
+td_sfc = (np.linspace(10, 0, 50) * np.ones((50, 50)));
+td_3d = np.zeros((len(pressure), 50, 50)) * units.kelvin
+for i, p in enumerate(pressure):
+    # Scale dewpoint colder and drier with height
+    scale = (p / 1000.0).magnitude ** 0.8 
+    td_3d[i, :, :] = (td_sfc * scale) * units.K
+     
+    
+td_4d = np.zeros((50, 50, 50, len(times)))
+for i, tm in enumerate(times):
+    td_4d[:, :, :, i] = t_3d + seasonal_variation[i] + np.random.uniform(-6, 3, size=(50, 50, 50)); 
+
+td_4d = td_4d * units.K
+td_4d = np.minimum(td_4d, t_4d)
+
+#Generate relative humidity from dewpoint
+rh = mpcalc.relative_humidity_from_dewpoint(t_4d, td_4d); 
 
 
 # place data into an xarray dataset object
@@ -131,6 +157,12 @@ theta = xr.DataArray(theta_4d, coords = coords, dims=['pressure', 'lat', 'lon', 
                      attrs={'standard_name' : 'Potential temperature', 'units' : 'K'})
 mixing_ratio = xr.DataArray(mixingRatio_4d, coords = coords, dims=['pressure', 'lat', 'lon', 'time'],
                             attrs={'standard_name' : 'Mixing Ratio', 'units': 'dimensionless'})
+vapor_pressure = xr.DataArray(vapor_pressure_4d, coords=coords, dims = ['pressure', 'lat', 'lon', 'time'],
+                              attrs={'standard name' : 'Vapor pressure', 'units' : 'hPa'})
+dewpoint = xr.DataArray(td_4d, coords=coords, dims = ['pressure', 'lat', 'lon', 'time'], 
+                        attrs = {'standard name': 'dewpoint', 'units' : 'K'})
+relative_humidity = xr.DataArray(rh, coords = coords, dims = ['pressure', 'lat', 'lon', 'time'],
+                                 attrs = {'standard name' : 'relative humidity', 'units' : '%'})
 
 ds = xr.Dataset({'uwind': uwind,
                    'vwind': vwind,
@@ -138,7 +170,10 @@ ds = xr.Dataset({'uwind': uwind,
                    'temperature': temperature, 
                    'height':height, 
                    'theta':theta,
-                   'mixing_ratio':mixing_ratio})
+                   'mixing_ratio':mixing_ratio,
+                   'vapor_pressure':vapor_pressure,
+                   'dewpoint':dewpoint, 
+                   'relative_humidity':relative_humidity})
 
 
 # Step 1: Initialize encoding dict for data variables
@@ -150,5 +185,9 @@ encoding = {
 # Step 2: Add compression settings for coordinates (no dtype conversion)
 for coord in ds.coords:
     encoding[coord] = {'zlib': True, 'complevel': 9}
+
+
+if os.path.exists("data_array_compressed.nc"):
+    os.remove("data_array_compressed.nc")
 
 ds.to_netcdf("data_array_compressed.nc", format="NETCDF4", encoding=encoding)
