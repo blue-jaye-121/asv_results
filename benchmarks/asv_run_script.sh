11ae943e98d43f95c408082dfa05ad15669230bb
blue-jaye-121 <savannahnorman121@gmail.com>
2025-06-26 13:55:57 -0600

    Update asv_run_script.sh - remove everything ab results repo

--- benchmarks/asv_run_script.sh
+++ benchmarks/asv_run_script.sh
@@ -9,13 +9,13 @@ CLONE_DIR="temp_repo_asv_run" # A temporary repo to clone to - deleted at the en
 #git clone "$RESULTS_REPO_URL" "$CLONE_DIR" TODO: Add this to Jenkinsfile
 
 #If the results folder exists, copy it into our main repo for asv running
-if [ -d "$CLONE_DIR"/results ]; then 
-    echo "copying results"
-    cp -r "$CLONE_DIR"/results/* "$ASV_RESULTS_PATH"
-else 
-    echo "cannot copy results"
-    exit 1
-fi
+#if [ -d "$CLONE_DIR"/results ]; then 
+#    echo "copying results"
+#    cp -r "$CLONE_DIR"/results/* "$ASV_RESULTS_PATH"
+#else 
+#    echo "cannot copy results"
+#    exit 1
+#fi
 
 #Call on the script to generate the commit hashes
 echo "----------------executing generate_hashes-------------------------"
@@ -33,13 +33,13 @@ asv machine --yes
 asv run HASHFILE:no_bot_merge_commits.txt -q -b other_benchmarks
    
 #Commit and push benchmark results to results repo
-if [ -d "$CLONE_DIR"/results ]; then #if the results_asv_results repo exits
-    rm -r "$CLONE_DIR"/results       #delete it 
-fi 
-cp -r "$ASV_RESULTS_PATH" "$CLONE_DIR"/results #copy the results to the results_asv_results repo
-cd "$CLONE_DIR"                                #move into the results repo
-git config --local user.email "script@noreply.com"
-git config --local user.name "ASV Script [bot]"
+#if [ -d "$CLONE_DIR"/results ]; then #if the results_asv_results repo exits
+#    rm -r "$CLONE_DIR"/results       #delete it 
+#fi 
+#cp -r "$ASV_RESULTS_PATH" "$CLONE_DIR"/results #copy the results to the results_asv_results repo
+#cd "$CLONE_DIR"                                #move into the results repo
+#git config --local user.email "script@noreply.com"
+#git config --local user.name "ASV Script [bot]"
 #git add results                                #TODO: Add following to Jenkinsfile
 #git commit -m "Update benchmark results"
 
