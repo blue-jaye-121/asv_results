e7cc6be1ca61d6aac42b803bf5fda4d3ffb7d35d
blue-jaye-121 <savannahnorman121@gmail.com>
2025-06-30 11:57:51 -0600

    Update entrypoint.sh - move into the repo before running benchmarks

--- benchmarks/entrypoint.sh
+++ benchmarks/entrypoint.sh
@@ -32,7 +32,8 @@ if [[ "$1" == "$RUN_BENCHMARKS" || "$1" == "$BENCHMARK_USER_LOGIN" ]]; then
 
     if [[ "$1" == "$RUN_BENCHMARKS" ]]; then
       # step-down from root and run benchmarks as benchmark user
-      exec pysu $(id -u -n $USER_ID) /container-benchmarks/benchmarks/runner.sh
+      cd container-benchmarks/
+      exec pysu $(id -u -n $USER_ID) benchmarks/runner.sh
     else
       # step-down from root and run bash (for exploration in interactive mode)
       exec pysu $(id -u -n $USER_ID) /bin/bash
