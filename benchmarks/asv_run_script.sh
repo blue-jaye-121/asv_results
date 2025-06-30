1af5158516ec146d6cf01fe7a2480b5137b7c3b2
blue-jaye-121 <savannahnorman121@gmail.com>
2025-06-30 16:25:54 -0600

    Update asv_run_script.sh - running on a real metpy hash

--- benchmarks/asv_run_script.sh
+++ benchmarks/asv_run_script.sh
@@ -15,7 +15,7 @@ asv machine --yes
 
 #Run asv
 #TODO: Make this everything & also a does not exist 
-asv run -b other_benchmarks
+asv run 9f59838^! -b other_benchmarks
    
 #Commit and push benchmark results to results repo
 #if [ -d "$CLONE_DIR"/results ]; then #if the results_asv_results repo exits
