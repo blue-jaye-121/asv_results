eda29c3dfeabc06c09023f424f42916eed00b1a3
Jaye Norman <savannahnorman121@gmail.com>
2025-07-02 07:35:47 -0600

    Skipping existing commits for ASV

--- benchmarks/asv_run_script.sh
+++ benchmarks/asv_run_script.sh
@@ -4,5 +4,5 @@
 #Set up asv machine
 asv machine --yes
 
-#TODO: Make this everything & also a does not exist 
-asv run HASHFILE:no_bot_merge_commits.txt
+# Runs asv on the commits in the hash file but skips ones that already have results
+asv run --skip-existing HASHFILE:no_bot_merge_commits.txt
